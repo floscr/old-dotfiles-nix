bac334aa8d2757fb1a9a632f18ff7f5b664b0c56
Florian Schroedl <flo.schroedl@gmail.com>
2019-12-12 20:53:09 +0100

    Add rust module

---
+++ modules/dev/rust.nix
@@ -0,0 +1,17 @@
+nk
+  { config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    rustc
+    cargo
+    cargo-generate
+
+    # stdenv deps
+    gcc
+    gnumake
+    openssl
+    pkgconfig
+    binutils
+  ];
+}
