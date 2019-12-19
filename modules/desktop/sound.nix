4e05a0217f00c3919dadd0f64f88b56207e029b7
Florian Schroedl <flo.schroedl@gmail.com>
2019-12-19 20:42:38 +0100

    Better pulseaudio sound

--- modules/desktop/sound.nix
+++ modules/desktop/sound.nix
@@ -42,7 +42,33 @@ in
     extraModules = [
       unstable.pulseaudio-modules-bt
     ];
+    # https://medium.com/@gamunu/enable-high-quality-audio-on-linux-6f16f3fe7e1f
+    daemon.config = {
+      default-sample-format = "float32le";
+      default-sample-rate = 48000;
+      alternate-sample-rate = 44100;
+      default-sample-channels = 2;
+      default-channel-map = "front-left,front-right";
+      default-fragments = 2;
+      default-fragment-size-msec = 125;
+      resample-method = "soxr-vhq";
+      enable-lfe-remixing = "no";
+      high-priority = "yes";
+      nice-level = -11;
+      realtime-scheduling = "yes";
+      realtime-priority = 9;
+      rlimit-rtprio = 9;
+      daemonize = "no";
+    };
   };
+
+  sound.extraConfig = ''
+    # Use PulseAudio plugin hw
+    pcm.!default {
+      type plug
+      slave.pcm hw
+    }
+  '';
 
   nixpkgs.config = {
     packageOverrides = pkgs: {
