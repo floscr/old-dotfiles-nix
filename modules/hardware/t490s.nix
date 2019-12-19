b087fa7e8010774662112f513df4155e44238971
Florian Schroedl <flo.schroedl@gmail.com>
2019-12-19 17:27:18 +0100

    Further hacking #t490s-piece-of-crap

--- modules/hardware/t490s.nix
+++ modules/hardware/t490s.nix
@@ -55,42 +55,33 @@
   services.tlp = {
     enable = true;
     extraConfig = ''
-      CPU_SCALING_GOVERNOR_ON_AC=performance
-      CPU_SCALING_GOVERNOR_ON_BAT=powersave
-      CPU_SCALING_MIN_FREQ_ON_AC=1600000
-      CPU_SCALING_MAX_FREQ_ON_AC=4800000
-      CPU_SCALING_MIN_FREQ_ON_BAT=400000
-      CPU_SCALING_MAX_FREQ_ON_BAT=3000000
-      CPU_HWP_ON_AC=balance_performance
-      CPU_HWP_ON_BAT=balance_power
-      CPU_MIN_PERF_ON_AC=0
-      CPU_MAX_PERF_ON_AC=100
-      CPU_MIN_PERF_ON_BAT=0
-      CPU_MAX_PERF_ON_BAT=80
-      CPU_BOOST_ON_AC=1
-      CPU_BOOST_ON_BAT=0
-      SCHED_POWERSAVE_ON_AC=0
-      SCHED_POWERSAVE_ON_BAT=1
-      ENERGY_PERF_POLICY_ON_AC=performance
-      ENERGY_PERF_POLICY_ON_BAT=powersave
-      PCIE_ASPM_ON_AC=performance
-      PCIE_ASPM_ON_BAT=powersave
-      DEVICES_TO_DISABLE_ON_DOCK="wifi"
-      DEVICES_TO_ENABLE_ON_UNDOCK="wifi"
-      '';
+    START_CHARGE_THRESH_BAT0=75
+    STOP_CHARGE_THRESH_BAT0=80
+    CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    ENERGY_PERF_POLICY_ON_BAT=powersave
+    '';
   };
 
-  # services.thinkfan.enable = true;
-  # services.thinkfan.sensors = ''
-  #   hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp3_input
-  #   hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp4_input
-  #   hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp1_input
-  #   hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp5_input
-  #   hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp2_input
-  #   hwmon /sys/devices/virtual/thermal/thermal_zone2/hwmon1/temp1_input
-  #   hwmon /sys/devices/virtual/thermal/thermal_zone3/hwmon2/temp1_input
-  #   hwmon /sys/devices/virtual/thermal/thermal_zone6/hwmon4/temp1_input
-  # '';
+  services.thinkfan.enable = true;
+  services.thinkfan.sensors = ''
+    hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp3_input
+    hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp4_input
+    hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp1_input
+    hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp5_input
+    hwmon /sys/devices/platform/coretemp.0/hwmon/hwmon3/temp2_input
+    hwmon /sys/devices/virtual/thermal/thermal_zone2/hwmon1/temp1_input
+    hwmon /sys/devices/virtual/thermal/thermal_zone6/hwmon4/temp1_input
+    hwmon /sys/devices/virtual/thermal/thermal_zone4/hwmon2/temp1_input
+  '';
+  services.thinkfan.levels = ''
+    (0, 0, 65)
+    (1, 50, 70)
+    (2, 68, 74)
+    (3, 72, 75)
+    (4, 74, 78)
+    (5, 76, 80)
+    (7, 78, 32767)
+ '';
 
   services.throttled = {
     enable = true;
@@ -104,9 +95,9 @@
       ## Settings to apply while connected to AC power
       [AC]
       # Update the registers every this many seconds
-      Update_Rate_s: 30
+      Update_Rate_s: 20
       # Max package power for time window #1
-      PL1_Tdp_W: 44
+      PL1_Tdp_W: 35
       # Time window #1 duration
       PL1_Duration_s: 28
       # Max package power for time window #2
@@ -114,7 +105,7 @@
       # Time window #2 duration
       PL2_Duration_S: 0.002
       # Max allowed temperature before throttling
-      Trip_Temp_C: 65
+      Trip_Temp_C: 63
       # Set HWP energy performance hints to 'performance' on high load (EXPERIMENTAL)
       HWP_Mode: True
       # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
