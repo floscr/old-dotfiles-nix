c8c85b507a80c640165fe77aed4665854477a438
Florian Schroedl <flo.schroedl@gmail.com>
2020-02-15 17:49:57 +0100

    Local cache file

--- modules/services/battery-notifier.nix
+++ modules/services/battery-notifier.nix
@@ -20,7 +20,7 @@ in
         '';
       };
       cacheFile = mkOption {
-        default = "/tmp/battery-notifier-cache";
+        default = "battery-notifier-cache";
         description = ''
           File to save the last battery status time, so you don't get the same nagging warning every minute.
         '';
@@ -53,14 +53,15 @@ in
       script = ''
         export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/capacity)
         export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${cfg.device}/status)
+        export cache_file = $XDG_CACHE_HOME/${cfg.cacheFile}
 
         # Remove cache file once the machine is charged again
-        if [[ $battery_capacity -ge ${builtins.toString cfg.notifyCapacity} && -f ${cfg.cacheFile} ]]; then
-          rm ${cfg.cacheFile}
+        if [[ ( $battery_status = "Charging" || $battery_capacity -ge ${builtins.toString cfg.notifyCapacity} ) && -f $cache_file ]]; then
+          rm $cache_file
         fi
 
-        if [[ $battery_capacity -le ${builtins.toString cfg.notifyCapacity} && $battery_status = "Discharging" && ! -f ${cfg.cacheFile} ]]; then
-            touch ${cfg.cacheFile}
+        if [[ $battery_capacity -le ${builtins.toString cfg.notifyCapacity} && $battery_status = "Discharging" && ! -f $cache_file ]]; then
+            touch $cache_file
             ${pkgs.libnotify}/bin/notify-send --urgency=critical --hint=int:transient:1 --icon=battery_empty "Battery Low" "You should probably plug-in."
         fi
 
