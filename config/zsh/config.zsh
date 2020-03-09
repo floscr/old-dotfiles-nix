90828b9ab80574250f627dc748324f510749a338
Florian Schroedl <flo.schroedl@gmail.com>
2020-03-09 17:53:48 +0100

    Copy over zsh config

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -1,6 +1,9 @@
-# Stop term/shell echoing back my input
-if [[ -n $INSIDE_EMACS ]]; then
-  unsetopt zle
+# Stop TRAMP (in Emacs) from hanging or term/shell from echoing back commands
+if [[ $TERM == dumb || -n $INSIDE_EMACS ]]; then
+  unsetopt zle prompt_cr prompt_subst
+  whence -w precmd >/dev/null && unfunction precmd
+  whence -w preexec >/dev/null && unfunction preexec
+  PS1='$ '
 fi
 
 ## General
