1796af9e6145b22ffd7f0bbb2b87c01bfe00017d
Florian Schroedl <flo.schroedl@gmail.com>
2020-03-09 18:56:41 +0100

    Update overlays

--- overlays.nix
+++ overlays.nix
@@ -6,6 +6,12 @@
           (builtins.fetchTarball
             https://github.com/xzfc/cached-nix-shell/archive/master.tar.gz) {});
     };
+
+    # Occasionally, "stable" packages are broken or incomplete, so access to the
+    # bleeding edge is necessary, as a last resort.
+    unstable = import <nixos-unstable> { inherit config; };
   })
+
+  (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
   (import ./overlays/chromium.nix)
 ]
