77cf039cadf871dbab420e2888bb49fd3395a9df
Florian Schroedl <flo.schroedl@gmail.com>
2020-03-20 17:26:30 +0100

    Add env variables to emacs

--- modules/services/emacsGit.nix
+++ modules/services/emacsGit.nix
@@ -3,14 +3,21 @@
 {
   systemd.user.services.emacs = {
     description = "Emacs Daemon";
-
+    environment = {
+      GTK_DATA_PREFIX = config.system.path;
+      GTK_PATH = "${config.system.path}/lib/gtk-3.0:${config.system.path}/lib/gtk-2.0";
+      NIX_PROFILES = "${pkgs.lib.concatStringsSep " " config.environment.profiles}";
+      TERMINFO_DIRS = "/run/current-system/sw/share/terminfo";
+      ASPELL_CONF = "dict-dir /run/current-system/sw/lib/aspell";
+      FSharpTargetsPath = "${pkgs.fsharp}/lib/mono/4.5/Microsoft.FSharp.Targets";
+    };
     serviceConfig = {
       Type = "forking";
       ExecStart = "${pkgs.emacsGit}/bin/emacs --daemon";
       ExecStop = "${pkgs.emacsGit}/bin/emacsclient --eval (kill-emacs)";
       Restart = "always";
     };
-
     wantedBy = [ "default.target" ];
   };
+  systemd.services.emacs.enable = true;
 }
