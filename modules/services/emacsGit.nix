5391dd805d82e754aacbea7632cd3a1a50a1dbbf
Florian Schroedl <flo.schroedl@gmail.com>
2020-03-20 20:11:12 +0100

    Fix stop command

--- modules/services/emacsGit.nix
+++ modules/services/emacsGit.nix
@@ -14,7 +14,7 @@
     serviceConfig = {
       Type = "forking";
       ExecStart = "${pkgs.emacsGit}/bin/emacs --daemon";
-      ExecStop = "${pkgs.emacsGit}/bin/emacsclient --eval (kill-emacs)";
+      ExecStop = "${pkgs.emacsGit}/bin/emacsclient --eval '(kill-emacs)'";
       Restart = "always";
     };
     wantedBy = [ "default.target" ];
