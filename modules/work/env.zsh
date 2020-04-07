9f3b406aac99f5e580eefa0721a9fd082724b42e
Florian Schroedl <flo.schroedl@gmail.com>
2020-04-07 18:28:21 +0200

    Move build bundle bin to regular bin dir

--- modules/work/env.zsh
+++ modules/work/env.zsh
@@ -4,13 +4,3 @@ export MEISTERLABS_DIR="$HOME/Code/Meisterlabs"
 export MEISTERLABS_MINDMEISTER_BE_DIR="$MEISTERLABS_DIR/mindmeister"
 export MEISTERLABS_MINDMEISTER_FE_DIR="$MEISTERLABS_DIR/mindmeister-web"
 export MEISTERLABS_MINDMEISTER_DOCKER_DIR="$MEISTERLABS_DIR/docker-dev-environment"
-
-mm-build-bundle() {
-    rm -rf build
-    nice -n10 npm run "$1"
-    notify-send "Bundle \"$1\" built."
-    docker exec -it mm-rails /bin/bash -c "rm -rf /opt/mindmeister-web && mkdir -p /opt/mindmeister-web"
-    docker cp build mm-rails:/opt/mindmeister-web
-    cd $MEISTERLABS_MINDMEISTER_DOCKER_DIR
-    ./mindmeister/rake "client:import_bundles_testing['$2']"
-}
