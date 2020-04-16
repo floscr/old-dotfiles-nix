ee88e4634a6c6a58ff2f057bcd7a6d75b0ecaf7f
Florian Schroedl <flo.schroedl@gmail.com>
2020-04-16 07:07:04 +0200

    Enable fstrim

--- hosts/thinknix/hardware-configuration.nix
+++ hosts/thinknix/hardware-configuration.nix
@@ -22,6 +22,10 @@ in {
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = true;
 
+  ## SSDs
+  services.fstrim.enable = true;
+
+  ### Harddrives
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/1256e32a-46cd-443e-9bb7-8fd910cc5b32";
       fsType = "ext4";
