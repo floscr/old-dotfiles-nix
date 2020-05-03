3171487b2b0a9bb7e4fbb7e82eff6c0d7ad11488
Florian Schroedl <flo.schroedl@gmail.com>
2020-05-03 20:54:55 +0200

    Fix polybar

--- modules/desktop/services/polybar.nix
+++ modules/desktop/services/polybar.nix
@@ -1,26 +1,33 @@
 { config, lib, pkgs, ... }:
 
-{
-  my.packages = with pkgs; [
-    (polybar.override {
-      mpdSupport = true;
-      pulseSupport = true;
-      nlSupport = true;
-    })
-  ];
-
-  systemd.user.services.polybar = {
-    description = "Polybar daemon";
-    wantedBy = [ "multi-user.target" ];
-    partOf = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.polybar}/bin/polybar main";
-      Restart = "always";
-      Type = "forking";
+let polybar = pkgs.polybar.override {
+  mpdSupport = true;
+  pulseSupport = true;
+  nlSupport = true;
     };
-  };
-
+in {
+  my.packages = [
+    polybar
+  ];
+  
   my.home.xdg.configFile = {
     "polybar" = { source = <config/polybar>; recursive = true; };
   };
+
+  systemd.user.services.polybar = {
+    description = "Polybar daemon";
+    after = [ "graphical-session-pre.target" ];
+    wantedBy = [ "graphical-session-pre.target" ];
+    partOf = [ "graphical-session-pre.target" ];
+    # restartTriggers = [ config.my.home.xdg.configFile."polybar".source ];
+    script = ''
+        polybar main &
+      '';
+    serviceConfig = {
+      Type = "forking";
+      Environment = "PATH=${polybar}/bin:/run/wrappers/bin:${pkgs.pulseaudio}/bin:${pkgs.gawk}/bin";
+      Restart = "on-failure";
+    };
+  };
+
 }
