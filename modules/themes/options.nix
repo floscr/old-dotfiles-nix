372859b59db1509062c8b557a074ca880cdb6cd8
Florian Schroedl <flo.schroedl@gmail.com>
2020-05-12 21:04:18 +0200

    Abstract theme to options

--- modules/themes/options.nix
+++ modules/themes/options.nix
@@ -8,7 +8,6 @@ with lib;
       type = with types; nullOr (either str path);
     };
 
-    # TODO Colors?
     font = mkOption {
       type = with types; submodule({ name, ... }: {
         options.primary = mkOption { type = str; };
@@ -18,6 +17,50 @@ with lib;
       };
     };
 
+    colors = mkOption {
+      type = with types; submodule({ name, ... }: {
+        options.black1 = mkOption { type = str; };
+        options.white = mkOption { type = str; };
+        options.grey1 = mkOption { type = str; };
+        options.grey2 = mkOption { type = str; };
+        options.red = mkOption { type = str; };
+        options.bred = mkOption { type = str; };
+        options.grn = mkOption { type = str; };
+        options.bgrn = mkOption { type = str; };
+        options.yellow = mkOption { type = str; };
+        options.byellow = mkOption { type = str; };
+        options.blue = mkOption { type = str; };
+        options.bblue = mkOption { type = str; };
+        options.mag = mkOption { type = str; };
+        options.bmag = mkOption { type = str; };
+        options.cyn = mkOption { type = str; };
+        options.bcyn = mkOption { type = str; };
+        # Aliases
+        options.terminalBackground = mkOption { type = str; };
+        options.text = mkOption { type = str; };
+      });
+      default = {
+        black1 = "#141517";
+        white = "#ffffff";
+        grey1 = "#c5c8c6";
+        grey2 = "#969896";
+        red = "#cc6666";
+        bred = "#de935f";
+        grn = "#b5bd68";
+        bgrn = "#757d28";
+        yellow = "##f0c674";
+        byellow = "#f9a03f";
+        blue = "#81a2be";
+        bblue = "#2a8fed";
+        mag = "#b294bb";
+        bmag = "#bc77a8";
+        cyn = "#8abeb7";
+        bcyn = "#a3685a";
+        # Aliases
+        terminalBackground = config.theme.colors.black1;
+        text = config.theme.colors.grey1;
+      };
+    };
   };
 
   config = {
