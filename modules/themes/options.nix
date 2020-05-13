8052416e71e2edfedcb900190c05528437b61a01
Florian Schroedl <flo.schroedl@gmail.com>
2020-05-13 18:35:14 +0200

    Add colors

--- modules/themes/options.nix
+++ modules/themes/options.nix
@@ -20,8 +20,10 @@ with lib;
     colors = mkOption {
       type = with types; submodule({ name, ... }: {
         options.black1 = mkOption { type = str; };
+        options.black2 = mkOption { type = str; };
         options.white = mkOption { type = str; };
         options.grey1 = mkOption { type = str; };
+        options.grey1blue = mkOption { type = str; };
         options.grey2 = mkOption { type = str; };
         options.red = mkOption { type = str; };
         options.bred = mkOption { type = str; };
@@ -37,18 +39,23 @@ with lib;
         options.bcyn = mkOption { type = str; };
         # Aliases
         options.terminalBackground = mkOption { type = str; };
+        options.background = mkOption { type = str; };
         options.text = mkOption { type = str; };
+        options.fail = mkOption { type = str; };
+        options.success = mkOption { type = str; };
       });
       default = {
         black1 = "#141517";
+        black2 = "#191c21";
         white = "#ffffff";
         grey1 = "#c5c8c6";
+        grey1blue = "#d8dee9";
         grey2 = "#969896";
         red = "#cc6666";
         bred = "#de935f";
         grn = "#b5bd68";
         bgrn = "#757d28";
-        yellow = "##f0c674";
+        yellow = "#f0c674";
         byellow = "#f9a03f";
         blue = "#81a2be";
         bblue = "#2a8fed";
@@ -58,6 +65,9 @@ with lib;
         bcyn = "#a3685a";
         # Aliases
         terminalBackground = config.theme.colors.black1;
+        background = config.theme.colors.black2;
+        success = config.theme.colors.grn;
+        fail = config.theme.colors.red;
         text = config.theme.colors.grey1;
       };
     };
