b19291a791b59b463983ab3bd54179161908e7ff
Florian Schroedl <flo.schroedl@gmail.com>
2020-05-13 18:34:40 +0200

    Style lockscreen

--- modules/desktop/services/lockscreen.nix
+++ modules/desktop/services/lockscreen.nix
@@ -3,24 +3,24 @@
 {
   environment.systemPackages = with pkgs; [
     i3lock-color
-    (pkgs.writeScriptBin "lockscreen" ''
+    (with config.theme.colors; pkgs.writeScriptBin "lockscreen" ''
       #! ${pkgs.zsh}/bin/zsh
 
-      background=292e39
-      insidecolor=2e3440ff
-      insidevercolor=81a1c1ff
-      insidewrongcolor=d08770ff
-      ringcolor=d8dee9ff
-      ringvercolor=d8dee9ff
-      ringwrongcolor=d8dee9ff
-      keyhlcolor=a3be8cff
-      bshlcolor=bf616aff
-      layoutcolor=eceff4ff
-      seperatorcolor=2e344000
-      timecolor=e5e9f0ff
-      datecolor=88c0d0ff
-      verifcolor=81a1c1ff
-      wrongcolor=d08770ff
+      background=${background}
+      insidecolor=${background}ff
+      insidevercolor=${background}ff
+      insidewrongcolor=${fail}ff
+      ringcolor=${grey1blue}1A
+      ringvercolor=${grey1blue}ff
+      ringwrongcolor=${fail}ff
+      keyhlcolor=${success}ff
+      bshlcolor=${fail}ff
+      layoutcolor=${grey1blue}ff
+      seperatorcolor=${black2}00
+      timecolor=${grey1blue}7f
+      datecolor=${cyn}ff
+      verifcolor=${blue}ff
+      wrongcolor=${fail}ff
 
       ${pkgs.i3lock-color}/bin/i3lock-color \
         -n \
@@ -41,36 +41,36 @@
         \
         --layoutcolor=$layoutcolor \
         --separatorcolor=$seperatorcolor \
-        --indpos="x+w/2:y+200+h/2" \
-        --radius=52 \
-        --ring-width=20 \
-        --noinputtext="" \
+        --indpos="x+w/2:y+h/1.4" \
+        --radius=50 \
+        --ring-width=14 \
+        --noinputtext="Enter Password" \
         \
         --clock \
         \
         --timecolor=$timecolor \
-        --timestr="•%H %M•" \
-        --timepos="x+w/2:y+64+h/2" \
-        --time-font="Fira Sans" \
-        --timesize=64 \
+        --timestr="%H:%M:%S" \
+        --timepos="x+w/2:y+h/1.8" \
+        --time-font="Fira Code" \
+        --timesize=22 \
         \
         --datecolor=$datecolor \
         --datestr="%A" \
-        --datepos="x+w/2:y+h/2" \
-        --date-font="Deadhead Script" \
-        --datesize=256 \
+        --datepos="x+w/2:y+h/1.95" \
+        --date-font="Fira Sans Bold" \
+        --datesize=57 \
         \
         --veriftext="Verifying" \
         --verifcolor=$verifcolor \
-        --verif-font="Deadhead Script" \
+        --verif-font="Fira Sans" \
         --verifpos="x+w/2:y+h/2" \
-        --verifsize=256 \
+        --verifsize=30 \
         \
         --wrongtext="Try again" \
         --wrongcolor=$wrongcolor \
-        --wrong-font="Deadhead Script" \
+        --wrong-font="Fira Sans" \
         --wrongpos="x+w/2:y+h/2" \
-        --wrongsize=256 &&\
+        --wrongsize=30 &&\
 
     '')
   ];
