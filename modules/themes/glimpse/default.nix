f60d314693ba8440238ef1d336637e5782e55138
Florian Schroedl <flo.schroedl@gmail.com>
2020-05-13 15:25:55 +0200

    Remove polybar from boot

--- modules/themes/glimpse/default.nix
+++ modules/themes/glimpse/default.nix
@@ -35,8 +35,6 @@
         recursive = true;
       };
       "xtheme/90-theme".source = ./Xresources;
-      # "dunst/dunstrc".source = ./dunstrc;
-      "bspwm/rc.d/polybar".source = ./polybar/run.sh;
       "gtk-3.0/gtk.css".source = ./gtk.css;
 
       # GTK
