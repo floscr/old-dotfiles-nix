c09eb40d836d0739695dec2ea5040ee73e331685
Florian Schroedl <flo.schroedl@gmail.com>
2020-06-02 16:31:13 +0200

    Remove mkBinding

--- modules/utils.nix
+++ modules/utils.nix
@@ -1,11 +1,2 @@
 rec {
-  mkBinding = { binding, command, description ? "" }: {
-      "sxhkd/sxhkdrc".text = ''
-        ${binding}
-          ${command}
-      '';
-      "rofi/cmds".text = ''
-        ${command};;;${binding};;;${description}
-      '';
-    };
 }
