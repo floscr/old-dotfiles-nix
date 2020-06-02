b15da5dbdac88f446c96efc3bd23c8721f5badae
Florian Schroedl <flo.schroedl@gmail.com>
2020-06-02 11:54:30 +0200

    Add work aliases

--- modules/work/env.zsh
+++ modules/work/env.zsh
@@ -4,3 +4,22 @@ export MEISTERLABS_DIR="$HOME/Code/Meisterlabs"
 export MEISTERLABS_MINDMEISTER_BE_DIR="$MEISTERLABS_DIR/mindmeister"
 export MEISTERLABS_MINDMEISTER_FE_DIR="$MEISTERLABS_DIR/mindmeister-web"
 export MEISTERLABS_MINDMEISTER_DOCKER_DIR="$MEISTERLABS_DIR/docker-dev-environment"
+
+function mm-sh {
+    docker exec -it mm-rails /bin/bash
+}
+
+function mm-load {
+    cd $MEISTERLABS_MINDMEISTER_DOCKER_DIR
+    ./mindmeister/rake load
+}
+
+function mm-bundle {
+    cd $MEISTERLABS_MINDMEISTER_DOCKER_DIR
+    ./mindmeister/bundle
+}
+
+function mm-attach {
+    cd $MEISTERLABS_MINDMEISTER_DOCKER_DIR
+    ./mindmeister/attach
+}
