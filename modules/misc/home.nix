103227a87f130e5bb7f928ef6cc5351d7057a16d
Florian Schroedl <flo.schroedl@gmail.com>
2020-06-07 20:40:50 +0200

    Add hue scripts

--- modules/misc/home.nix
+++ modules/misc/home.nix
@@ -1,15 +1,46 @@
 { config, lib, pkgs, ... }:
 
-{
-  environment.systemPackages = with pkgs; [
+let
+  livingRoomCenter = "8";
+  kitchenCeiling = "2";
+  kitchenBoard = "10";
+  entry = "3";
+  livingRoomStandMiddle = "4";
+  livingRoomStandLast = "5";
+  bedRoomCeil = "11";
+  bedRoomLed = "13";
+in {
+  my.packages = with pkgs; [
     hue-cli
+    (pkgs.writeScriptBin "hue_living_room_full" ''
+      #!${pkgs.stdenv.shell}
+      hue ${livingRoomCenter} on;
+      hue ${livingRoomCenter} brightness 100% &
+      hue ${livingRoomStandMiddle} on
+      hue ${livingRoomStandMiddle} brightness 100% &
+      hue ${livingRoomStandLast} on
+      hue ${livingRoomStandLast} brightness 100% &
+    '')
+    (pkgs.writeScriptBin "hue_living_room_read" ''
+      #!${pkgs.stdenv.shell}
+      hue ${livingRoomCenter} off&
+      hue ${livingRoomStandMiddle} on
+      hue ${livingRoomStandMiddle} brightness 10% &
+      hue ${livingRoomStandLast} on
+      hue ${livingRoomStandLast} brightness 100%&
+    '')
   ];
 
   my.bindings = [
     {
-      description = "Living Room Center 100%";
+      description = "Living Room Full";
       categories = "Hue Home";
-      command = "hue 8 on; hue 8 brightness 100%";
+      command = ''hue_living_room_full'';
+    }
+    {
+      description = "Living Room Read";
+      categories = "Hue Home";
+      command = ''hue_living_room_full'';
     }
   ];
 }
