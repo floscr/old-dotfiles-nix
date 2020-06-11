111c193b78c4525c802ab033f217d412b5a6103f
Florian Schroedl <flo.schroedl@gmail.com>
2020-06-11 15:57:56 +0200

    Move nimx to bin

--- modules/dev/nim.nix
+++ modules/dev/nim.nix
@@ -3,9 +3,20 @@
 {
   environment.systemPackages = with pkgs; [
       nim
+      (pkgs.writeScriptBin "nimx" ''
+        bin=$1
+        src="$bin.nim"
+        shift
+
+        if [[ ! -f "$bin" || "$src" -nt "$bin" ]]; then
+            echo "Compiling $src..."
+            ${pkgs.nim} c --verbosity:0 --hint[Processing]:off --excessiveStackTrace:on $src
+        fi
+
+        $bin $@
+    '')
   ];
   my = {
     # env.PATH = [<nimbin>];
-    zsh.rc = lib.readFile <config/nim/aliases.zsh>;
   };
 }
