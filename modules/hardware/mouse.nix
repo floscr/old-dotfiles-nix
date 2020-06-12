e1b5186cd735223df041481c44147ac8adaf6c37
Florian Schroedl <flo.schroedl@gmail.com>
2020-06-12 13:16:06 +0200

    Indent

--- modules/hardware/mouse.nix
+++ modules/hardware/mouse.nix
@@ -11,16 +11,16 @@
   services.xserver.synaptics = {
     enable = true;
     additionalOptions = ''
-        Option "VertScrollDelta" "100"
-        Option "HorizScrollDelta" "100"
-        Option "ClickTime" "25"
-        Option "Sensitivity" "1.4"
-        Option "FingerHigh" "12"
-        Option "FingerLow" "1"
-        Option "BottomEdge" "30"
-        Option "PalmSize" "45"
-        Option "IgnoreThumb" "true"
-      '';
+      Option "VertScrollDelta" "100"
+      Option "HorizScrollDelta" "100"
+      Option "ClickTime" "25"
+      Option "Sensitivity" "1.4"
+      Option "FingerHigh" "12"
+      Option "FingerLow" "1"
+      Option "BottomEdge" "30"
+      Option "PalmSize" "45"
+      Option "IgnoreThumb" "true"
+    '';
     palmDetect = true;
     minSpeed = ".9";
     maxSpeed = "1.4";
