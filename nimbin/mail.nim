489bf9c459eaa912bc7fb1cc6a8fc4cd89eae454
Florian Schroedl <flo.schroedl@gmail.com>
2020-06-18 10:59:13 +0200

    Update color and symbol

--- nimbin/mail.nim
+++ nimbin/mail.nim
@@ -6,17 +6,28 @@ import sugar
 let mailDir = expandTilde("~/.mail")
 let accounts = ["Work/Inbox/new"]
 
-proc newCount() =
+let color = "#f0c674"
+let prefix = ""
+
+proc polyBarPrettyPrint(count: int): string =
+  if count == 0:
+    return ""
+  else:
+    return &"%{{F{color}}}{prefix} {count}%{{F-}}"
+
+proc newCount(): int =
   let files = accounts
     .map((x) => joinPath(mailDir, x))
     .map((x) => walkDir(x, true) |> toSeq |> len)
     .foldl(a + b)
     
-  echo files
+  return files
 
 var p = newParser("mail"):
   command("newCount"):
     run:
-      newCount()
+      newCount() |>
+      polyBarPrettyPrint |>
+      echo
 
 p.run(@["newCount"])
