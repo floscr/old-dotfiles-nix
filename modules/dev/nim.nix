50a5670f764c6fa7c7eb3496a7ce4352acd4fe25
Florian Schroedl <flo.schroedl@gmail.com>
2020-08-10 22:05:52 +0200

    Restore nimx

--- modules/dev/nim.nix
+++ modules/dev/nim.nix
@@ -3,8 +3,17 @@
 {
   environment.systemPackages = with pkgs; [
       unstable.nim
+      (pkgs.writeScriptBin "nimx" ''
+        bin=$1
+        src="$bin.nim"
+        shift
+
+        if [[ ! -f "$bin" || "$src" -nt "$bin" ]]; then
+            echo "Compiling $src..."
+            ${pkgs.nim}/bin/nim c -r --verbosity:0 --hint[Processing]:off --excessiveStackTrace:on -d:release $src
+        fi
+
+        $bin $@
+    '')
   ];
-  my = {
-    # env.PATH = [<nimbin>];
-  };
 }
