1a5356c1eb522b82b724624f67590bbd85223faa
Florian Schroedl <flo.schroedl@gmail.com>
2020-08-28 23:55:18 +0200

    Move version to device

--- hosts/thinknix/hardware-configuration.nix
+++ hosts/thinknix/hardware-configuration.nix
@@ -201,4 +201,10 @@ in {
   swapDevices =
     [ { device = "/dev/disk/by-uuid/03ad0fe9-be47-438f-a5c0-88eca0287fde"; }
     ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
 }
