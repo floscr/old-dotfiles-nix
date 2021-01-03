a2153318aa8ba03fb19f211aaa1eab815d061423
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-03 10:37:58 +0100

    Add theme options

--- modules/themes/options.nix
+++ modules/themes/options.nix
@@ -8,68 +8,37 @@ with lib;
       type = with types; nullOr (either str path);
     };
 
-    font = mkOption {
-      type = with types; submodule({ name, ... }: {
-        options.primary = mkOption { type = str; };
-      });
-      default = {
-        primary = "Iosevka";
-      };
-    };
+    font = mkOptStr "Iosevka";
 
     colors = mkOption {
-      type = with types; submodule({ name, ... }: {
-        options.black1 = mkOption { type = str; };
-        options.black2 = mkOption { type = str; };
-        options.white = mkOption { type = str; };
-        options.grey1 = mkOption { type = str; };
-        options.grey1blue = mkOption { type = str; };
-        options.grey2 = mkOption { type = str; };
-        options.red = mkOption { type = str; };
-        options.bred = mkOption { type = str; };
-        options.grn = mkOption { type = str; };
-        options.bgrn = mkOption { type = str; };
-        options.yellow = mkOption { type = str; };
-        options.byellow = mkOption { type = str; };
-        options.blue = mkOption { type = str; };
-        options.bblue = mkOption { type = str; };
-        options.mag = mkOption { type = str; };
-        options.bmag = mkOption { type = str; };
-        options.cyn = mkOption { type = str; };
-        options.bcyn = mkOption { type = str; };
+      type = with types; nullOr submodule({ name, ... }: {
+        options.black1 = str;
+        options.black2 = str;
+        options.white = str;
+        options.grey1 = str;
+        options.grey1blue = str;
+        options.grey2 = str;
+        options.red = str;
+        options.bred = str;
+        options.grn = str;
+        options.bgrn = str;
+        options.yellow = str;
+        options.byellow = str;
+        options.blue = str;
+        options.bblue = str;
+        options.mag = str;
+        options.bmag = str;
+        options.cyn = str;
+        options.bcyn = str;
+
         # Aliases
-        options.terminalBackground = mkOption { type = str; };
-        options.background = mkOption { type = str; };
-        options.text = mkOption { type = str; };
-        options.fail = mkOption { type = str; };
-        options.success = mkOption { type = str; };
+        options.terminalBackground = str;
+        options.background = str;
+        options.text = str;
+        options.fail = str;
+        options.success = str;
       });
-      default = {
-        black1 = "#141517";
-        black2 = "#191c21";
-        white = "#ffffff";
-        grey1 = "#c5c8c6";
-        grey1blue = "#d8dee9";
-        grey2 = "#969896";
-        red = "#cc6666";
-        bred = "#de935f";
-        grn = "#b5bd68";
-        bgrn = "#757d28";
-        yellow = "#f0c674";
-        byellow = "#f9a03f";
-        blue = "#81a2be";
-        bblue = "#2a8fed";
-        mag = "#b294bb";
-        bmag = "#bc77a8";
-        cyn = "#8abeb7";
-        bcyn = "#a3685a";
-        # Aliases
-        terminalBackground = config.theme.colors.black1;
-        background = config.theme.colors.black2;
-        success = config.theme.colors.grn;
-        fail = config.theme.colors.red;
-        text = config.theme.colors.grey1;
-      };
+      default = {};
     };
   };
 }
